e9d86ce017134b5bb11189fc6c63eeece8bb4e8a
Zhenya_SD <En198806044815162342>
2019-03-15 13:58:07 +0300

    test

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.scss';
+
+class Header extends React.Component{
+    render() {
+        return(
+            <h2>Hello <span>test</span></h2>
+        )
+    }
+}
+
+export default Header;
+
