8ca91d22a4f9917b408b4d1038da3190b7d41596
evgenijvojtehovic <evgeniy.vaytekhovich@service-it.by>
2019-03-22 14:55:16 +0300

    Исправил работоспособность функций внутри класса

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Header.scss';
 
 class Header extends React.Component{
+
+    test = () => {
+        console.log('test');
+    };
+
     render() {
         return(
-            <h2>Hello <span><i>ttt</i>test</span></h2>
+            <h2 onClick={this.test}>Hello <span><i>ttt</i>test</span></h2>
         )
     }
 }
